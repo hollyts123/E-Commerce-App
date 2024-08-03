233ab89177b5f65f3a8e004d6aabc1574bb76b14
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2024-08-04 01:19:35 +0200

    created SearchBar component and module.css for it

---
+++ app/component/searchBar/SearchBar.tsx
@@ -0,0 +1,14 @@
+"use client";
+import searchBarStyles from './SearchBar.module.css';
+
+interface SearchBarProps {
+    className?: string;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({className}) => {
+    return (
+        <div className={className}>
+            
+        </div>
+    )
+}
