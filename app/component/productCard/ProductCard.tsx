a2d6e9b3d4358637ab58f28f686f345fa6b31bac
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2024-08-05 02:54:37 +0200

    added Product interface, products.json, modified the ProductCard component, updated page.tsx

--- app/component/productCard/ProductCard.tsx
+++ app/component/productCard/ProductCard.tsx
@@ -2,12 +2,14 @@
 import productStyles from './ProductCard.module.css';
 import Rating from '../rating/Rating';
 import { useState } from 'react';
+import Product from '../../interfaces/Product';
 
 interface ProductCardProps {
     className?: string;
+    product: Product;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ className }) => {
+const ProductCard: React.FC<ProductCardProps> = ({ className, product }) => {
     const [isLiked, setIsLiked] = useState(false);
     const handleLikeClick = () => {
         setIsLiked(!isLiked);
@@ -15,16 +17,16 @@ const ProductCard: React.FC<ProductCardProps> = ({ className }) => {
     return (
         <div className={className}>
             <div className={productStyles['product-wrap']}>
-            <img className={productStyles['product-img']} src="/products/product.svg" alt="Product image" />
+            <img className={productStyles['product-img']} src={product.image} alt="Product image" />
             <Rating className={productStyles.rating} />
             <img className={productStyles['like-img']}
                 src={isLiked ? '/productCard/liked.svg' : '/productCard/like.svg'}
                 alt='Heart icon'
                 onClick={handleLikeClick}
             />
-            <p className={productStyles['product-name']} >Cotton shirt Regular Fit</p>
+            <p className={productStyles['product-name']}>{product.name}</p>
             <div className={productStyles['price-wrap']}>
-                <p className={productStyles.price}>RM 100.00</p>
+                <p className={productStyles.price}>{product.price}</p>
                 <img src='/productCard/bag.svg' alt='Bag icon' />
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,36 @@
+'use client';
 import ProductCard from './component/productCard/ProductCard';
 import SearchBar from './component/searchBar/SearchBar';
+import Product from './interfaces/Product';
+import productsData from '../products.json';
+import { useState } from 'react';
 
 export default function Home() {
+  const [visibleProducts, setVisibleProducts] = useState(8);
+  const productsToShow = productsData.slice(0, visibleProducts);
+
+  const handleViewAll = () => {
+    setVisibleProducts((prevVisibleProducts) => prevVisibleProducts + 4);
+  }
+
   return (
     <div className="main">
       <SearchBar className='search-bar' />
+      <div className='recommended-wrap'>
+        <h3 className='recommended'>Recommended</h3>
+
+        {/* {visibleProducts < productsData.length && (
+          <p className='view-all-btn' onClick={handleViewAll}>View All</p>
+        )} */}
+
+        <button className='view-all-btn' onClick={handleViewAll}>View All</button>
+
+      </div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-2 product-card-container">
-        <ProductCard className='product-card' />
-        <ProductCard className='product-card' />
-        <ProductCard className='product-card' />
-        <ProductCard className='product-card' />
-        <ProductCard className='product-card' />
-        <ProductCard className='product-card' />
-        <ProductCard className='product-card' />
-        <ProductCard className='product-card' />
+        {
+          productsToShow.map((product: Product) => (
+            <ProductCard key={product.id} className="product-card" product={product} />
+          ))}
       </div>
     </div>
   );
